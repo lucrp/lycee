42089d677bdcf35bd21f85c0d11df85c39bb17d9
lucasrp <lcsrpaiva@gmail.com>
2021-05-05 17:56:56 +0200

    Update res.partner (teachers) view and model

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,6 +5,7 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _name = "res.partner"
     _inherit = "res.partner"
+
     name = fields.Char('Nom', required=True)
     class_ids = fields.Many2many("iut.class", string="Classes")
     _sql_constraints = [
